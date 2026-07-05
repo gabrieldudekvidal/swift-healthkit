b2c0e45063881f8f91f46f037ac70e042e4e7331
Gabriel Vidal <gabrieldkvl@icloud.com>
2026-07-05 08:53:24 +0800

    Initial Commit

---
+++ HealthKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthKit
+//
+//  Created by Gabriel Vidal on 2026-07-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthKit/HealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitApp.swift
+//  HealthKit
+//
+//  Created by Gabriel Vidal on 2026-07-05.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
